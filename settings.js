515510fc3ad25f3c08ab4421fb93b8c0e324bbdb
~Tycoon <peterma642@gmail.com>
2025-04-05 18:41:12 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 //Feel free to customize my bot informations to yours. Starting from this point
 //owmner v card
-global.ytname = "YT: ElitePro" //ur yt chanel name
-global.socialm = "GitHub: Elite-MD" //ur github or insta name
-global.location = "Port Harcourt, Nigeria" //ur location
+global.ytname = "YT: Tøsh-Ke_XMD-V1" //ur yt chanel name
+global.socialm = "GitHub: Tøsh-Ke_XMD-V1" //ur github or insta name
+global.location = "Nairobi, Kenya" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'Tøsh-Ke_XMD-V1 //ur bot name
+global.ownernumber = '254792580026' //ur owner number
+global.ownername = 'Tøsh-Ke_XMD-V1 //ur owner name
 global.websitex = "https://youtube.com/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
 
-global.themeemoji = '👨🏼‍💻'
+global.themeemoji = '✌️'
 global.wm = "ELITE-PRO-V2"
 global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
 global.packname = "Sticker By"
